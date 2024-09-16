da7cc18dc441d4539cc99afc980dd5af65ff3dce
HAROON NISSAR KHANDAY <136074700+Khandayharoon@users.noreply.github.com>
2024-09-16 12:40:09 +0530

    dfhf

--- server.js
+++ server.js
@@ -54,6 +54,10 @@ app.get('/user/:username', async (req, res) => {
 });
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${ PORT }`);
 });
